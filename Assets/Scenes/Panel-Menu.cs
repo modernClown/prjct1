0e5e092e9f21c6c1b2ed3e579ce31f08a87531de
modernClown <62207808+modernClown@users.noreply.github.com>
2020-05-16 01:14:17 +0300

    init

---
+++ Assets/Scenes/Panel-Menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Mainmenu()
+    {
+        SceneManager.LoadScene("Mainmenu", LoadSceneMode.Additive);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
+public class Window : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
